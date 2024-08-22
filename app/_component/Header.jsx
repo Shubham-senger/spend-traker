6b5541c67673fad5472aacf10f0f3eeb925c6884
Shubham Sengar <shubhamsengar2303@gmail.com>
2024-08-22 19:54:25 +0530

    Added clerk Auth

--- app/_component/Header.jsx
+++ app/_component/Header.jsx
@@ -1,8 +1,13 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
+    const {user,isSignedIn}=useUser();
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
     
@@ -11,7 +16,13 @@ function Header() {
             width={160}
             height={100}
         />
+        {isSignedIn?
+        <UserButton/>:
+        <Link href={'/sign-in'}>
         <Button>Get Started</Button>
+        </Link>
+
+        }
     </div>
   )
 }
